4bc655b89ba70511d682375e7ed49e99af560264
Sébastien Lamy <lamyseba@free.fr>
2017-05-02 13:03:50 +0200

    correction du nom de fichier base de données dont il faut faire le backup

--- CodeSource/github_backup.sh
+++ CodeSource/github_backup.sh
@@ -6,7 +6,7 @@
 # de mot de passe pour l'utilisateur qui lance ce script
 
 cd /home/calandreta/Bibli
-git add tellico.rc
+git add inventaire.tc
 git commit -m "Mise à jour de la base de donnée tellico"
 git push origin master
 
