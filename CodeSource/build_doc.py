ab450584fdd4565e43f500bdf4f0a1d8b98e3d34
Sébastien Lamy <lamyseba@free.fr>
2017-05-12 04:42:05 +0200

    Conversion des ancres Nettoyage des fichiers html générés avant fusion master

--- CodeSource/build_doc.py
+++ CodeSource/build_doc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys, os, glob, markdown, re
+import sys, os, glob, markdown, re, codecs
 from string import Template
 
 
@@ -12,12 +12,22 @@ md=markdown.Markdown(
     extensions=['markdown.extensions.codehilite',
                 'markdown.extensions.fenced_code',
                 'markdown.extensions.toc'
-               ]
+               ],
     extension_configs = {
         'markdown.extensions.codehilite': {
             'css_class': 'highlight'
         }
     })
+
+
+def ext_and_slug(match):
+    modified_link = match.group(1)+".html"
+    if match.group(2):
+        modified_link += '#'+markdown.extensions.toc.slugify(match.group(2),'-')
+    print("link conversion:",match.group(0))
+    print("              ->",modified_link)
+    return modified_link
+
     
 # Les ancres générées par github peuvent contenir des accents, pas celles
 # générées par python_markdown. Donc pour la version locale de la documentation
@@ -27,24 +37,20 @@ md=markdown.Markdown(
 # fichiers .md, avec une ancre dans le lien
 # parenthèse. Ex:[impression.py](CodeSource/README.md#impression.py)
 # alp signifie anchor link pattern
-alp = re.compile('(\]\(.*)\.md#?(.*)\))')
+alp = re.compile('(\]\s*\(.*)\.md#?([^\)]*)')
 
 # Une autre expression, cette fois pour trouver des liens en note.
 # Ex: [impression.py]:CodeSource/Readme.md#impression.py
 # Il faut supprimer les accents des ancres dans la version html
-alp2 =re.compile('\]\s*:.*)\.md#?(.*)$')
+alp2 =re.compile('(\]\s*:.*)\.md#?(.*)')
 
-"""
-for root,dirs,files,fdroot in os.fwalk(app_home_dir):
-    for file_name in fnmatch.filter(files,'*.md'):
-        css_link_path=os.path.join(root,file_name)
-        print(css_link_path)
-"""
+
 
 # file_paths: la liste de tous les chemin des fichiers .md, relatif au
 # répertoire de travail.
 file_paths = glob.glob("**/*.md",recursive=True)
 for file_path in file_paths :
+    print("---")
     # on calcule l'emplacement du fichier de mise en forme en fonction
     # du chemin du fichier .md
     css_link= ("../"*file_path.count(os.path.sep))+style_sheet_path
@@ -55,29 +61,31 @@ for file_path in file_paths :
     md_in=""
     with open(os.path.join(os.getcwd(),file_path),'r') as f:
         md_in = f.read()    
-    # transforme les ancres dans le fichier markdown
-    md_out=alp.sub(r'\1.html#\3',md_in)
-    md_out=alp2.sub(md_out)
+    # transforme les ancres dans le fichier markdown  
+    md_out=alp.sub(ext_and_slug,md_in)
+    md_out=alp2.sub(ext_and_slug, md_out) 
     # transforme le texte markdown en html
     html_out = md.convert(md_out)
     
     # ouvre le template de documention
-    template_in=""
+    template_in = ""
     with open(os.path.join(os.getcwd(),layout_dir,"index.html"),'r') as f:
-        template = Template(f.read())
+        template_in = f.read()
+    template = Template(template_in)
     # substitue les champs à substituer
-    template.substitute(
+    template_out = template.substitute(
         markdown_output=html_out,
         css_link=css_link
-    )
-        
-   # crée le fichier html avec le contenu final
-   with codecs.open(html_file_name,'w',encoding="utf-8", errors="xmlcharrefreplace") as f
+    )       
+    # crée le fichier html avec le contenu final
+    with codecs.open(html_file_name,'w',encoding="utf-8", errors="xmlcharrefreplace") as f:
         f.write(template_out)
-        
+    md.reset()
+    print()
     
 
 """
+DEPRECATED: tentative de fonctionner avec la commande sed
 f=open('/tmp/calandreta_'+self.item_type+'.html','w')
         args=[  'xsltproc',
                 '--param','entry_predicate','"'+self.entry_predicate+'"',
